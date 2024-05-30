456c8cc60f33ea1d7d2a14269644c97633b45224
Mertali Tosun <mertali2635@icloud.com>
2024-05-30 23:03:23 +0300

    final

--- public/js/advert-card.js
+++ public/js/advert-card.js
@@ -1,15 +1,35 @@
 const accordionButtons = document.querySelectorAll(".accordion-button");
+const advertDetailBtn = document.querySelectorAll(".advertDetailBtn");
 
 accordionButtons.forEach((accordionButton) => {
     accordionButton.addEventListener("click", () => {
         const targetId = accordionButton.getAttribute("data-bs-target");
         const advertDetailBtn = document.querySelector(`#advertDetailBtn[data-bs-target="${targetId}"]`);
         if (advertDetailBtn) {
-            if (advertDetailBtn.innerHTML.trim() === "Detayları Göster") {
+            if (advertDetailBtn.innerHTML.trim() === "Detayları Göster" || advertDetailBtn.innerHTML.trim() === "İncele") {
                 advertDetailBtn.innerHTML = "Detayları Gizle";
-            } else {
+            } else if(advertDetailBtn.innerHTML.trim() === "Detayları Gizle") {
                 advertDetailBtn.innerHTML = "Detayları Göster";
             }
         }
     });
 });
+
+advertDetailBtn.forEach((accordionButton) => {
+    accordionButton.addEventListener("click", () => {
+        const targetId = accordionButton.getAttribute("data-bs-target");
+        const advertDetailBtn = document.querySelector(`#advertDetailBtn[data-bs-target="${targetId}"]`);
+        if (advertDetailBtn) {
+            if (advertDetailBtn.innerHTML.trim() === "Detayları Göster") {
+                advertDetailBtn.innerHTML = "Detayları Gizle";
+            } else if(advertDetailBtn.innerHTML.trim() === "Detayları Gizle"){
+                advertDetailBtn.innerHTML = "Detayları Göster";
+            }
+        }
+    });
+});
+
+
+const click = () =>{
+    
+}
